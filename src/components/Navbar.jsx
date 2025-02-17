21d92b7d86fda9c4de516c6756626a0c53e811de
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-02-17 13:36:58 +0600

    navbar implement is done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import {
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
 import Link from "next/link";
+import { Switch } from "@/components/ui/switch";
 
 const Navbar = () => {
   return (
@@ -19,18 +20,68 @@ const Navbar = () => {
           </div>
           <NavigationMenu>
             <NavigationMenuList>
-              <NavigationMenuItem>
+              <NavigationMenuItem className="flex items-center space-x-8">
                 <NavigationMenuLink href="/">Home</NavigationMenuLink>
+                <NavigationMenuLink href="/rooms">Rooms</NavigationMenuLink>
+                <NavigationMenuLink href="/gallery">Gallery</NavigationMenuLink>
+                <NavigationMenuLink href="/blog">Blog</NavigationMenuLink>
+                <NavigationMenuLink href="/page">Page</NavigationMenuLink>
+                <NavigationMenuLink href="/contact">Contact</NavigationMenuLink>
                 <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
                 <NavigationMenuContent>
-                  <NavigationMenuLink>Link ewrrrrrrrrrrrrr</NavigationMenuLink>
-                  <NavigationMenuLink>Link ewrrrrrrrrrrrrr</NavigationMenuLink>
-                  <NavigationMenuLink>Link ewrrrrrrrrrrrrr</NavigationMenuLink>
-                  <NavigationMenuLink>Link ewrrrrrrrrrrrrr</NavigationMenuLink>
+                  <ul className="row-0 w-[220px] shadow-md rounded-md px-5 py-4 space-y-2 right-0">
+                    <li>
+                      <NavigationMenuLink
+                        href="/roomCards"
+                        className="hover:text-pink-500"
+                      >
+                        Room Cards
+                      </NavigationMenuLink>
+                    </li>
+                    <li>
+                      <NavigationMenuLink
+                        href="/roomCarousel"
+                        className="hover:text-pink-500"
+                      >
+                        Room Carousel
+                      </NavigationMenuLink>
+                    </li>
+                    <li>
+                      <NavigationMenuLink
+                        href="/layout1"
+                        className="hover:text-pink-500"
+                      >
+                        Single Room - Layout 1
+                      </NavigationMenuLink>
+                    </li>
+                    <li>
+                      <NavigationMenuLink
+                        href="/layout2"
+                        className="hover:text-pink-500"
+                      >
+                        Single Room - Layout 2
+                      </NavigationMenuLink>
+                    </li>
+                    <li>
+                      <NavigationMenuLink
+                        href="/layout3"
+                        className="hover:text-pink-500"
+                      >
+                        Single Room - Layout 3
+                      </NavigationMenuLink>
+                    </li>
+                  </ul>
                 </NavigationMenuContent>
               </NavigationMenuItem>
             </NavigationMenuList>
           </NavigationMenu>
+          {/* dark and light mode  */}
+          <div>
+            <div className="flex items-center">
+              <span className="mr-2">Dark And Light</span>
+              <Switch />
+            </div>
+          </div>
         </nav>
       </header>
     </div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -59,3 +59,4 @@ export default {
   },
   plugins: [require("tailwindcss-animate")],
 };
+
