f004d2ea2d823686daede025d837be08b4e8802d
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-02-24 02:21:08 +0600

    image implement is done in feature section

--- src/app/page.js
+++ src/app/page.js
@@ -1,43 +1,70 @@
 import Banner from "@/components/Banner";
-import Image from 'next/image'
-import welcomeImg from '@/assets/banner/welcome.jpg'
+import Image from "next/image";
+import welcomeImg from "@/assets/welcome.jpg";
 import { GoArrowLeft } from "react-icons/go";
 import { GoArrowRight } from "react-icons/go";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import feature1 from "@/assets/feature1.jpg";
+import feature2 from "@/assets/feature2.jpg";
+import feature3 from "@/assets/feature3.jpg";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       {/* welcome section  */}
-      <div className="mb-12 mt-24">
+      <div className="mt-24">
         <div className="lg:flex justify-between items-center max-w-7xl mx-auto px-4 md:px-8">
-          <div className="w-full lg:w-1/2 text-center space-y-2">
+          <div className="w-full lg:w-1/2 text-center space-y-2 mb-5 lg:mb-0">
             <div className="flex items-center justify-center gap-1">
-            <GoArrowLeft className="mt-1 text-gray-400"/>
-            <p className="text-lg text-gray-400">Welcome to Star</p>
-            <GoArrowRight className="mt-1 text-gray-400"/>
+              <GoArrowLeft className="mt-1 text-gray-400" />
+              <p className="text-lg text-gray-400">WELCOME TO STAR</p>
+              <GoArrowRight className="mt-1 text-gray-400" />
             </div>
-            <h2 className="text-3xl md:text-4xl font-semibold text-gray-600">Finest Luxury Hotels & Resort</h2>
+            <h2 className="text-3xl md:text-4xl font-semibold text-gray-800">
+              Finest Luxury Hotels & Resort
+            </h2>
             <p className="text-gray-400">
               Star Hotels and Resorts is a distinctive collection of hotels
               chosen for their rich heritage, enviable central locations and
               personalized guest experience. Each is unique in the story it
               tells, a story revealed through its history, architecture and
-              character. This is the star legacy: inimitable hotels in
-              coveted locations, run by talented people dedicated to the art of
+              character. This is the star legacy: inimitable hotels in coveted
+              locations, run by talented people dedicated to the art of
               hospitality.
             </p>
-            <Button variant="destructive" className="text-lg">See More</Button>
+            <Button variant="destructive" className="text-lg">
+              See More
+            </Button>
           </div>
           <div className="w-full lg:w-1/2 mx-auto">
-            <Image className="object-cover" src={welcomeImg} alt=""/>
+            <Image className="object-cover" src={welcomeImg} alt="" />
           </div>
         </div>
       </div>
       {/* feature section  */}
-      <div className="my-12">
-        <div className="lg:flex justify-between items-center max-w-full mx-auto px-4 md:px-8"></div>
+      <div className="my-24">
+        <div className="flex items-center justify-center gap-1">
+          <GoArrowLeft className="mt-1 text-gray-400" />
+          <p className="text-lg text-gray-400">ENJOY LUXURY PRIVILEGES</p>
+          <GoArrowRight className="mt-1 text-gray-400" />
+        </div>
+        <h2 className="text-3xl md:text-4xl font-semibold text-gray-800 text-center mt-2">
+          Featured Specials
+        </h2>
+        <div className="lg:flex justify-between items-center max-w-7xl mx-auto px-4 md:px-8 mt-12">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            <div>
+            <Image className="object-cover" src={feature1} alt=""/>
+            </div>
+           <div>
+           <Image className="object-cover" src={feature3} alt=""/>
+           </div>
+            <div>
+            <Image className="object-cover" src={feature2} alt=""/>
+            </div>
+            </div>
+        </div>
       </div>
     </div>
   );
